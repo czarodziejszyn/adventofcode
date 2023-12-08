54044cb748f4e096533f09c082667ab1b3a2b10f
sryszka <01178585@pw.edu.pl>
2023-12-08 16:13:04 +0100

    czasy

--- 2023/day6/run.sh
+++ 2023/day6/run.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#/bin/sh
 javac part1.java
 javac part2.java
 java part1 input.txt
+start_time=$(date "+%Y-%m-%d %H:%M:%S.%3N")
+echo "Start Time: $start_time"
 java part2 input.txt
+end_time=$(date "+%Y-%m-%d %H:%M:%S.%3N")
+echo "Finish Time: $end_time"
 rm part1.class part2.class
--- 2023/day8/run.sh
+++ 2023/day8/run.sh
@@ -5,7 +5,9 @@ javac part2.java
 #start_time=$(date "+%Y-%m-%d %H:%M:%S.%3N")
 #echo "Start Time: $start_time"
 #java part1 input.txt
+start_time=$(date "+%Y-%m-%d %H:%M:%S.%3N")
+echo "Start Time: $start_time"
 java part2 input.txt
-#finish_time=$(date "+%Y-%m-%d %H:%M:%S.%3N")
-#echo "Finish Time: $finish_time"
+finish_time=$(date "+%Y-%m-%d %H:%M:%S.%3N")
+echo "Finish Time: $finish_time"
 rm Node.class part2.class #part1.class
